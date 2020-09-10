1658a4aefc58b6ddccf18b0eeafc50bf01af55c7
Julliann Dulay <jullianndulay@gmail.com>
2020-09-09 18:16:40 -0700

    mvp met

--- src/main/java/com/lambdaschool/orders/controllers/OrderController.java
+++ src/main/java/com/lambdaschool/orders/controllers/OrderController.java
@@ -4,11 +4,14 @@ package com.lambdaschool.orders.controllers;
 import com.lambdaschool.orders.models.Order;
 import com.lambdaschool.orders.services.OrderService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
+
+import javax.validation.Valid;
+import java.net.URI;
 
 @RestController
 public class OrderController {
@@ -21,5 +24,40 @@ public class OrderController {
     public ResponseEntity<?> findOrderById(@PathVariable long ordnum){
         Order myOrder = orderService.findOrderById(ordnum);
         return new ResponseEntity<>(myOrder, HttpStatus.OK);
+    }
+
+    //POST REQ
+    //http://localhost:2019/orders/order
+    @PostMapping(value = "/orders/order", consumes = "application/json",
+            produces =
+            "application/json")
+    public ResponseEntity<?> addNewOrder(@Valid @RequestBody Order newOrder){
+        newOrder = orderService.save(newOrder);
+        newOrder.setOrdnum(0);
+        HttpHeaders responseHeaders = new HttpHeaders();
+        URI newOrderURI =
+                ServletUriComponentsBuilder.fromCurrentRequest().path(
+                        "/{ordnum}").buildAndExpand(newOrder.getOrdnum()).toUri();
+        responseHeaders.setLocation(newOrderURI);
+        return new ResponseEntity<>(null,responseHeaders,HttpStatus.CREATED);
+    }
+
+    //PUT REQ
+    //http://localhost:2019/orders/order/63
+    @PutMapping( value = "/orders/order/{ordnum}", consumes = "application" +
+            "/json", produces = "application/json")
+    public ResponseEntity<?> updateWholeOrder(@PathVariable long ordnum,
+                                              @Valid @RequestBody Order updateOrder){
+        updateOrder.setOrdnum(ordnum);
+        updateOrder = orderService.save(updateOrder);
+        return new ResponseEntity<>(HttpStatus.OK);
+    }
+
+    //Delete Req
+    //http://localhost:2019/orders/order/58
+    @DeleteMapping(value = "/orders/order/{ordnum}")
+    public ResponseEntity<?> deleteOrderByNum(@PathVariable long ordnum){
+        orderService.delete(ordnum);
+        return new ResponseEntity<>(HttpStatus.OK);
     }
 }
